57581fd670dbf832ff8d3df230e35e6acd6dda38
AGROYGBV <amandagracerenfro@gmail.com>
2021-12-21 15:28:02 -0600

    added delete button functionality for pallete and add/delete box counter

---
+++ public/pallete.js
@@ -0,0 +1,33 @@
+
+const deleteBtn = document.querySelector('#delete-btn')
+
+const boxCounter = document.querySelector('#box-counter')
+
+const id = window.location.pathname.split('/palletes/')[1]
+
+//delete pallete button
+deleteBtn.addEventListener('click', async () => {
+    let res = await fetch (`/palletes/${id}`, {
+        method: 'DELETE'
+    })
+    console.log(res)
+    window.location.assign('/palletes')
+});
+
+//add and delete boxes button
+async function box(value) {
+    let currentBoxes = parseInt(boxCounter.innerHTML)
+    console.log(currentBoxes)
+    currentBoxes += value
+    boxCounter.innerHTML = currentBoxes
+    let res = await fetch (`/sauces/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            boxes: currentBoxes
+        })
+    })
+}
+
