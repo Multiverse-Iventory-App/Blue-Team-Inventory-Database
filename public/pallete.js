00d31fc8af42e2b1fa738d8a5edf98bff5acf251
AGROYGBV <amandagracerenfro@gmail.com>
2021-12-22 21:56:27 -0600

    reseeded

--- public/pallete.js
+++ public/pallete.js
@@ -3,36 +3,50 @@ const boxCounter = document.querySelector('#box-counter')
 
 const maxCapacity = document.querySelector('#max-capacity').innerHTML
 const currentCapacity = document.querySelector('#current-capacity').innerHTML
+// const id = document.querySelector('#pallete-id').innerHTML;
 let availableCapacity = document.querySelector('#available-capacity').innerHTML;
 
 const id = window.location.pathname.split('/palletes/')[1]
+
+
 
 let value = parseInt(maxCapacity) - parseInt(currentCapacity)
 availableCapacity = value
 
 //delete pallete button
-deleteBtn.addEventListener('click', async () => {
+deleteBtn.addEventListener('click', async (id) => {
+    console.log(id)
     let res = await fetch (`/pallete/${id}`, {
         method: 'DELETE'
     })
     console.log(res)
-    //window.location.assign('/warehouse/')
+    //window.location.assign('/warehouses/${id}')
 });
 
+// async function deletedPallete(id){
+//     //delete a sauce matching parameter id
+//     let res = await fetch(`/palletes/${id}` ,{
+//         method: 'DELETE'
+//     })
+//     console.log(res)
+//     //send user back to the sauces path
+//     window.location.assign('/warehouse')
+// }
+
 //add and delete boxes button
-async function box(value) {
-    let currentBoxes = parseInt(boxCounter.innerHTML)
-    console.log(currentBoxes)
-    currentBoxes += value
-    boxCounter.innerHTML = currentBoxes
-    let res = await fetch (`/palletes/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            boxes: currentBoxes
-        })
-    })
-}
+// async function box(value) {
+//     let currentBoxes = parseInt(boxCounter.innerHTML)
+//     console.log(currentBoxes)
+//     currentBoxes += value
+//     boxCounter.innerHTML = currentBoxes
+//     let res = await fetch (`/palletes/${id}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             boxes: currentBoxes
+//         })
+//     })
+// }
 
